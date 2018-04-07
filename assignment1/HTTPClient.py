482bfe4f58d96359ee58d963845921e1f4b6cd43
StevenMHernandez <contact@stevenhdesigns.com>
2018-04-07 10:17:46 -0400

    add lots of comments, make a bit more readable

--- assignment1/HTTPClient.py
+++ assignment1/HTTPClient.py
@@ -7,42 +7,66 @@ import re
 
 class HTTPClient:
     def GET(self, url):
+        # Validate URL
         if not self.__validate_url(url):
             exit("ERR - arg 1")
+
+        # parse url into self.host self.path
         self.__parse_url(url)
+
+        # build the request string
         request_string = self.__build_request_header("GET")
 
         print(request_string)  # required print statement
+
+        # send the request string and get response
         response_string = self.__send_request(request_string)
+
+        # get header data from response
         parsed_data = self.__parse_response_header(response_string)
+
+        # print specific header data as specified for the project
         self.print_parsed_data(parsed_data)
+
+        # store the response IF we received a 2xx response
+        # these files are stored prefixed with a "__" to distinguish them from other files in the system.
+        # the stored file name will be "__{hostname}{filepath}"
         if 200 <= parsed_data['response_code'] < 300:
             self.store_body(parsed_data['body'])
 
     def PUT(self, url, file_path):
+        # Validate parameters (url and local filepath)
         if not self.__validate_url(url):
             exit("ERR - arg 2")
         if not os.path.isfile(file_path):
             exit("ERR - arg 3")
+
+        # Get local file
         file_content = "\r\n".join(open(file_path).readlines())
+
+        # parse url into self.host self.path
         self.__parse_url(url)
+
+        # build the request string
+        # this request must append header with a body of file content
         request_string = self.__build_request_header("PUT") + file_content + "\r\n\r\n"
 
         print(request_string)  # required print statement
-        response_string = self.__send_request(request_string)
-        parsed_data = self.__parse_response_header(response_string)
-        self.print_parsed_data(parsed_data)
-        if 200 <= parsed_data['response_code'] < 300:
-            self.store_body(parsed_data['body'])
 
-    def put(self):
-        pass
+        # send the request string and get response
+        response_string = self.__send_request(request_string)
+
+        # get header data from response
+        parsed_data = self.__parse_response_header(response_string)
+
+        # print specific header data as specified for the project
+        self.print_parsed_data(parsed_data)
 
     def __validate_url(self, url):
         return url.startswith("http://")
 
     def __parse_url(self, url):
-        # http://hostname[:port][/path]
+        # expecting http://hostname[:port][/path]
         m = re.match(r'http://([A-z0-9.]+)(:([0-9]+))?(/.*)?', url)
 
         # m.group(0) contains the whole url string, so we ignore it
@@ -67,18 +91,32 @@ class HTTPClient:
             exit("ERR - Connection refused")
 
     def __parse_response_header(self, response_string):
+        # Parse all metadata from the response string
+        # Parsing (response_code, server) for all requests
+        # For 2xx responses, we also parse (last-modified-date, content-length)
+        # For 3xx responses, we als parse (redirect-location)
+        # Additionally, for all of these parsed header values, I've set defaults of "Unknown"
+        # because occasionally servers do not respond with all of the expected header components.
         header, body = response_string.split("\r\n\r\n", 1)
+
+        # Get response code
         m = re.search(r'HTTP\/1\.[0-9] ([0-9]+)', header)
         response_code = int(m.group(1))
+
+        # Get server name/type
         m = re.search('Server: (.*?)\r\n', header)
-        server = m.group(1)
+        server = m.group(1) if m is not None else "Unknown"
 
         # parse response header for 2xx responses
         if 200 <= response_code < 300:
+            # Get last modified date
             m = re.search(r'Last-Modified: (.*?)\r\n', header)
             last_modified_date = m.group(1) if m is not None else "NOT SPECIFIED!"
+
+            # Get content length
             m = re.search(r'Content-Length: (.*?)\r\n', header)
             content_length = m.group(1) if m is not None else "NOT SPECIFIED!"
+
             return {
                 "response_code": response_code,
                 "server": server,
@@ -90,8 +128,10 @@ class HTTPClient:
 
         # parse response header for 3xx responses
         if 300 <= response_code < 400:
+            # Get redirect location
             m = re.search(r'Location: (.*?)\r\n', header)
             location = m.group(1)
+
             return {
                 "response_code": response_code,
                 "server": server,
@@ -110,22 +150,25 @@ class HTTPClient:
         }
 
     def print_parsed_data(self, parsed_data):
-        print("Response Code: {}".format(parsed_data['response_code']))
-        print("Server: {}".format(parsed_data['server']))
+        print(parsed_data['response_code'])  # required output
+        print(parsed_data['server'])  # required output
 
         # Print fields from 2xx response codes
         if 200 <= parsed_data['response_code'] < 300:
-            print("Last-Modified: {}".format(parsed_data['last_modified_date']))
-            print("Content-Length: {}".format(parsed_data['content_length']))
+            print(parsed_data['last_modified_date'])  # required output
+            print(parsed_data['content_length'])  # required output
 
         # Print fields from 3xx response codes
         if 300 <= parsed_data['response_code'] < 400:
-            print("Location: {}".format(parsed_data['location']))
+            print(parsed_data['location'])  # required output
 
         # Print the whole header
-        print("\n" + parsed_data['header'])
+        print("\n" + parsed_data['header'])  # required output
 
     def store_body(self, body):
+        # store file with appended "__" to distinguish it from the other files on the server
+        # there weren't strong specification on how to store the file, so storing it with filename:
+        # "__{hostname}{filepath}"
         filename = "__" + (".".join((self.hostname + self.path).split("/")))
         f = open(filename, "w+")
         f.write(body)
@@ -150,8 +193,6 @@ if __name__ == "__main__":
         print(client.PUT(args[2], args[3]))
         exit(0)
 
-    print("ERR - arguments supplied do not match expected signature.")
-    print("Try:")
-    print("python HTTPClient.py {url}")
-    print("python HTTPClient.py PUT {url} {local-file-path}")
+    # If we don't match the above command signatures, then the command wasn't entered as expected
+    print("Usage: java HTTPClient URL or java HTTPClient PUT URL path/<filename>")
     exit(255)
--- assignment1/HTTPServer.py
+++ assignment1/HTTPServer.py
@@ -17,46 +17,83 @@ class HTTPServer:
         try:
             welcome_socket.listen(1)
 
-            print('Listening on port', port)
-
+            # Loop forever to continue receiving connections until the program is terminated
             while 1:
-                # Waits for some client to connect and creates new socket
-                # for connection
+                # Waits for some client to connect and creates new socket for the connection
                 connection_socket, addr = welcome_socket.accept()
-                print('Client Made Connection')
                 client_ip, client_port = addr
 
                 # Read input line from socket
                 client_sentence = connection_socket.recv(1024)
-                print('FROM CLIENT:', client_sentence)
 
-                # Determine next action to take
+                # Determine next action to take, "GET" or "PUT"?
                 response_string = self.determine_response(client_ip, client_port, client_sentence)
 
                 # Write output line to socket
                 connection_socket.send(response_string)
-                print('TO CLIENT', response_string)
 
                 # Close the connection socket
                 connection_socket.close()
         finally:
+            # When we close the command in the terminal,
+            # make sure we automatically close the welcome socket as well.
             welcome_socket.close()
+
+    def GET(self, client_sentence):
+        # parse file_path from the request
+        file_path = self.get_file_path_from_client_sentence(client_sentence, "GET")
+
+        # Return 404 if the file does not exist currently
+        if not os.path.isfile(file_path):
+            return self.build_response(404, "Not Found", "404 Not Found")
+
+        # Otherwise, we can return the file
+        file_contents = "\r\n".join(open(file_path).readlines())
+
+        return self.build_response(200, "OK", file_contents)
+
+    def PUT(self, client_sentence):
+        try:
+            # parse file_path from the request
+            file_path = self.get_file_path_from_client_sentence(client_sentence, "PUT")
+
+            # get the file content from the PUT request
+            uploaded_file_content = client_sentence.split("\r\n\r\n", 1)[1]
+
+            # Store the file at the specified location
+            # NORMALLY, we wouldn't want to accept any file path here.
+            # A malicious user could specify the path to be "../../super-secret-file.txt"
+            # We wouldn't want the user to be able to access these parent directories
+            # but for this assignment we will ignore these sorts of conditions.
+            file = open(file_path, "w+")
+            file.write(uploaded_file_content)
+            file.close()
+
+            return self.build_response(200, "OK File Created", "File Created")
+        except:
+            # If there are any errors reading the file and returning the file
+            # Then we can return a 606
+            # We can reach this response IF the file_path is actually just a directory path
+            return self.build_response(606, "FAILED File NOT Created", "File NOT Created")
 
     def _validate_port(self, port):
         if 0 >= port or port >= 65536:
             exit("ERR - arg 1")
 
     def determine_response(self, client_ip, client_port, client_sentence):
-        server_response = ""
+        # Determine if "GET" or "PUT" method. Otherwise, throw error and exit.
         method = self.get_method(client_sentence)
 
-        print("{ip}:{port}:{method}".format(ip=client_ip, port=client_port, method=method))
-        print(client_sentence)
+        server_response = None
+
+        print("{ip}:{port}:{method}".format(ip=client_ip, port=client_port, method=method))  # required output
+        print(client_sentence)  # required output
 
         if method == "GET":
             server_response = self.GET(client_sentence)
         if method == "PUT":
             server_response = self.PUT(client_sentence)
+
         return server_response
 
     def get_method(self, client_sentence):
@@ -73,34 +110,9 @@ class HTTPServer:
             "Content-Length: {}".format(len(response_body)),
             "Connection: close",
             "Content-Type: text/html;charset=utf-8",
-            "",
+            "", # required to automatically have \r\n\r\n between header and body
             response_body,
         ])
-
-    def GET(self, client_sentence):
-        file_path = self.get_file_path_from_client_sentence(client_sentence, "GET")
-
-        # Return 404 if the file does not exist currently
-        if not os.path.isfile(file_path):
-            return self.build_response(404, "Not Found", "404 Not Found")
-
-        # Otherwise, we can return the file
-        file_contents = "\r\n".join(open(file_path).readlines())
-
-        return self.build_response(200, "OK", file_contents)
-
-    def PUT(self, client_sentence):
-        try:
-            file_path = self.get_file_path_from_client_sentence(client_sentence, "PUT")
-            uploaded_file_content = client_sentence.split("\r\n\r\n", 1)[1]
-
-            file = open(file_path, "w+")
-            file.write(uploaded_file_content)
-            file.close()
-
-            return self.build_response(200, "OK File Created", "File Created")
-        except:
-            return self.build_response(606, "FAILED File NOT Created", "File NOT Created")
 
     def get_file_path_from_client_sentence(self, client_sentence, method):
         regex = r'GET (.*) HTTP' if method == "GET" else r'PUT (.*) HTTP'
@@ -116,7 +128,6 @@ class HTTPServer:
         # which could potentially return any file on the server even if we don't want this.
         # Again, we will ignore the security aspects of this because this server will only
         # ever be run locally.
-        print("." + path)
         return "." + path
 
 
@@ -126,5 +137,3 @@ if __name__ == "__main__":
 
     server = HTTPServer()
     server.listen(int(args[1]))
-
-    print("\n\n\n\n")
